2d520b43b25b1d1d448a5e2f97c69b1e372aaa0a
numbBrain <pwned_id@protonmail.com>
2020-05-23 19:54:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# yiff.party scraper
+python scraper to download media from yiff.party
+
+### Requirements
+Only **requests and lxml** packages are required. Do:
+```
+pip install -r requirements.txt
+```
+
+### Usage:
+
+```
+$ python yiff_scraper.py --help
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --links LINKS [LINKS ...]
+                        take links from STDI
+  --read file.txt       read links from file
+  --dest [DEST]         specify download directory
+  --timeout [TIMEOUT]   timeout in seconds for requests
+  --delay [DELAY]       seconds to wait between downloads
+  --continous           paginate automatically and scrap next pages
+  --version             show program's version number and exit
+
+  ```
+### Example
+  ```
+  $ python yiff_scraper.py --read links.txt 
+  ```
+
+  above command reads links from the file links.txt, one link each line. Downloads files from each link into the current folder. The **default timeout is 60 seconds** and **default wait time between requests is 5 seconds**
+
+```
+  $ python yiff_scraper.py --links link1 link2 --dest Downloads --timeout 120 --delay 10
+```
+
+  above command takes links(link1, link2,...) from the terminal and downloads files into the Downloads folder. Timeout for requests is set as 120 seconds and wait time between requests is set as 10 seconds
+
+```
+  $ python yiff_scraper.py --links link1 link2 --dest ~/Documents --timeout 120 --delay 10 --continous
+```
+
+  this command takes links from the terminal. Files are downloaded into the **Documents** folder of the current user **(in Linux)**. Timeout for requests is set as 120 seconds and wait time between requests is set as 10 seconds. **--continous option does pagination automatically, it enables you to scrape next pages until the last page is reached.**
+
+  **--continous option is ideal for scraping creator's complete profile**
+
+### Note
+
+* If scraping is interrupted, start the scraper again without altering the downloaded files. This will resume scraping instead of scraping from the start.
+
+* Use proper timeout and sleep durations to avoid the script from crashing often.
+
+* Failed URLs will be a displayed on the terminal without interupting scraping and are also added to  **failed_links.txt** file.
+
+* Filenames of files are the same as uploaded by the creators. But, filenames are prefixed with a number(counter) to keep filenames unique.
+
+## Limitaions
+
+* Given the current situation, this scraper does not perform async requests, intentionally. `aiohttp` support shall be added in the future.
+
+* Only Downloads files hosted at `data.yiff.party`. Does not download embeded videos, etc.
---
+++ yiff_scraper.py
@@ -0,0 +1,167 @@
+import os
+import requests
+import socket
+import argparse
+from lxml import html
+from time import sleep 
+from requests.adapters import HTTPAdapter
+from urllib.parse import urlparse
+from urllib3.exceptions import ReadTimeoutError
+from requests.packages.urllib3.util.retry import Retry
+
+def failedLinks(link, failedDirectory):
+
+    with open(failedDirectory, "a") as failed:
+        failed.write(suppliedLink+"\n")
+
+parser = argparse.ArgumentParser(description="scrape files from yiff.party posts")
+groupsParser = parser.add_mutually_exclusive_group()
+groupsParser.add_argument("--links", type=str, nargs="+", const=None, help="take links from STDI")
+groupsParser.add_argument("--read", metavar="file.txt", type=argparse.FileType("r", encoding="UTF-8"), const=None, help="read links from file")
+parser.add_argument("--dest" ,type=str, nargs="?", default=os.getcwd(), help="specify download directory")
+parser.add_argument("--timeout", type=int, nargs="?", default=60, help="timeout in seconds for requests")
+parser.add_argument("--delay", type=int, nargs="?", default=5, help="seconds to wait between downloads")
+parser.add_argument("--continous", action="store_true", default=False, help="paginate automatically and scrap next pages")
+parser.add_argument("--version", action="version", version="yiff scraper 1.0")
+args = parser.parse_args()
+
+SKIP = "https_www.dropbox.com_static_images_spectrum-icons_generated_content_content-folder_dropbox-large.png"
+HEADERS = { "User-Agent" : "Opera/9.80 (Linux armv7l) Presto/2.12.407 Version/12.51 , D50u-D1-UHD/V1.5.16-UHD (Vizio, D50u-D1, Wireless)"}
+RETRIES = Retry(total=10, backoff_factor=3)
+ERASE = "\033[2K"
+DOMAIN = "yiff.party"
+TIMEOUT = args.timeout
+SLEEP = args.delay
+CONTINUE = args.continous
+DESTINATION = os.path.abspath(args.dest)
+FILE_COUNTER = 0
+
+#check if atleast one option is passed
+if not (args.read or args.links):
+    print("[-] No options specified, use --help for available options")
+    exit()
+
+else:
+    if args.links:
+        suppliedLinks = list(dict.fromkeys(args.links)) 
+    else:
+        suppliedLinks = list(dict.fromkeys(list(args.read.read().splitlines())))
+        args.read.close()
+print(f"[+] Download folder: {DESTINATION}\n")
+
+with requests.Session() as session:
+    session.mount("https://", HTTPAdapter(max_retries=RETRIES)) #HTTP adapter mount to attempt retries
+
+    for suppliedLink in suppliedLinks:
+
+        suppliedLink = suppliedLink.strip()
+        if not urlparse(suppliedLink).netloc == DOMAIN: #check if URl is of yiff.party, if not, ignore it
+            print(f"[-] {suppliedLink} doesn't belong to {DOMAIN}, skipping it")
+            continue
+
+        print(f"[*] Getting page: {suppliedLink}", end="\r", flush=True)
+
+        try:
+            pageResp = session.get(suppliedLink, headers=HEADERS, timeout=TIMEOUT)
+            pageResp.raise_for_status()
+
+        except requests.exceptions.ConnectionError as connErr:
+            failedLinks(suppliedLink, failedDirectory)
+            print(connErr)
+            continue
+    
+        except (socket.timeout, ReadTimeoutError, requests.Timeout) as timeoutErr:
+            failedLinks(suppliedLink, failedDirectory)
+            print(timeoutErr)
+            continue
+
+        except requests.exceptions.HTTPError as err:
+            failedLinks(suppliedLink, failedDirectory)
+            print(err)
+            continue
+        
+        print(ERASE, end="\r", flush=True)
+        print(f"[+] {suppliedLink}, page retrieved\n")            
+        pageTree = html.fromstring(pageResp.text)
+        pageTree.make_links_absolute(suppliedLink)
+        creatorName = pageTree.xpath("//span[@class='yp-info-name']/text()")[0].strip()
+        patreonName = pageTree.xpath("//span[@class='yp-info-name']/small/text()")[0].strip()
+        creatorName = creatorName+patreonName
+        creatorDirectory = os.path.join(DESTINATION, creatorName)
+        failedDirectory = os.path.join(creatorDirectory, "failed_links.txt")
+        os.makedirs(creatorDirectory, exist_ok=True)
+        if CONTINUE:
+            nextPage = pageTree.xpath("//a[@class='btn pag-btn pag-btn-bottom'][1]/@href")
+            if nextPage: 
+                nextPage = nextPage[0]
+                index = suppliedLinks.index(suppliedLink) + 1
+                suppliedLinks.insert(index, nextPage)         
+
+        allMedia = pageTree.xpath("//div[@class='card-attachments']//a/@href")
+        if not allMedia:
+            allMedia = pageTree.xpath("//div[@class='card-action']//a/@href")
+
+        for media in allMedia:
+
+            if SKIP in media:
+                continue
+            
+            filename = f"{FILE_COUNTER}_{media.strip('/').split('/')[-1]}"
+            filepath = os.path.join(creatorDirectory, filename)
+
+            try:
+                fileData = session.head(media, headers=HEADERS, timeout=TIMEOUT)
+                try:
+                    fileSize = int(fileData.headers["Content-Length"])
+                except KeyError:
+                    fileSize = 0
+                if os.path.isfile(filepath):
+
+                    localFileSize = os.stat(filepath).st_size
+                    if localFileSize == fileSize:
+                        FILE_COUNTER += 1
+                        continue
+                    else:
+                        diff = int(fileSize) - int(localFileSize)
+                        if not diff == int(fileSize):
+                            HEADERS["Range"] = f"bytes={diff}-{fileSize}"
+
+                        fileResp = session.get(media, headers=HEADERS, stream=True, timeout=TIMEOUT)
+                        fileResp.raise_for_status()
+                else:
+                    fileResp = session.get(media, headers=HEADERS, stream=True, timeout=TIMEOUT)
+                    fileResp.raise_for_status()
+
+            except requests.exceptions.ConnectionError as connErr:
+                failedLinks(media, failedDirectory)
+                print(connErr)
+                continue
+        
+            except (socket.timeout, ReadTimeoutError, requests.Timeout) as timeErr:
+                failedLinks(media, failedDirectory)
+                print(timeErr)
+                continue
+        
+            except requests.exceptions.HTTPError as err:
+                failedLinks(media, failedDirectory)
+                print(err)
+                continue
+        
+            
+            with open(filepath, "wb") as file:
+
+                print(ERASE, end="\r", flush=True)
+                print(f"[+] Downloading file: {filename}, have downloaded {FILE_COUNTER} files", end="\r", flush=True)
+
+                for iterData in fileResp.iter_content(chunk_size=2**20): 
+                    if iterData:
+                        file.write(iterData) 
+                
+                FILE_COUNTER += 1
+                if FILE_COUNTER % 4 == 0 :
+                    sleep(int(SLEEP) + 5)
+                else:
+                    sleep(SLEEP)
+                    
+print(ERASE, end="\r", flush=True)
+print(f"[+] {FILE_COUNTER} files downloaded")
