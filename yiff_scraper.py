d9c6fc0e850763244a051835c4ebcba3a6e3af41
numbBrain <pwned_id@protonmail.com>
2020-05-27 07:46:31 +0530

    changed range requests

--- yiff_scraper.py
+++ yiff_scraper.py
@@ -3,16 +3,21 @@ import requests
 import socket
 import argparse
 from lxml import html
-from time import sleep 
-from requests.adapters import HTTPAdapter
+from time import sleep
 from urllib.parse import urlparse
+from requests.adapters import HTTPAdapter
 from urllib3.exceptions import ReadTimeoutError
 from requests.packages.urllib3.util.retry import Retry
 
-def failedLinks(link, failedLogPath):
+def writeFiles(content, failedLogPath):
 
-    with open(failedLogPath, "a") as failed:
-        failed.write(suppliedLink+"\n")
+    with open(failedLogPath, "a") as logFile:
+
+        if isinstance(content, list):
+            for line in content:
+                logFile.write(line+"\n")
+        elif isinstance(content, str):
+            logFile.write(content+"\n")
 
 parser = argparse.ArgumentParser(description="scrape files from yiff.party posts")
 groupsParser = parser.add_mutually_exclusive_group()
@@ -35,8 +40,8 @@ SLEEP = args.delay
 CONTINUE = args.continous
 DESTINATION = os.path.abspath(args.dest)
 FILE_COUNTER = 0
+FILENAME_COUNTER = 1
 
-#check if atleast one option is passed
 if not (args.read or args.links):
     print("[-] No options specified, use --help for available options")
     exit()
@@ -47,15 +52,16 @@ else:
     else:
         suppliedLinks = list(dict.fromkeys(list(args.read.read().splitlines())))
         args.read.close()
+
 print(f"[+] Download folder: {DESTINATION}\n")
 
 with requests.Session() as session:
-    session.mount("https://", HTTPAdapter(max_retries=RETRIES)) #HTTP adapter mount to attempt retries
+    session.mount("https://", HTTPAdapter(max_retries=RETRIES)) 
 
     for suppliedLink in suppliedLinks:
 
         suppliedLink = suppliedLink.strip()
-        if not urlparse(suppliedLink).netloc == DOMAIN: #check if URl is of yiff.party, if not, ignore it
+        if not urlparse(suppliedLink).netloc == DOMAIN: 
             print(f"[-] {suppliedLink} doesn't belong to {DOMAIN}, skipping it")
             continue
 
@@ -78,87 +84,107 @@ with requests.Session() as session:
             continue
         
         print(ERASE, end="\r", flush=True)
-        print(f"[+] {suppliedLink}, page retrieved\n")            
+        print(f"[+] {suppliedLink}, page retrieved\n")
+
         pageTree = html.fromstring(pageResp.text)
         pageTree.make_links_absolute(suppliedLink)
+        posts = pageTree.xpath("//div[@class='card large yp-post']")
+
         creatorName = pageTree.xpath("//span[@class='yp-info-name']/text()")[0].strip()
         patreonName = pageTree.xpath("//span[@class='yp-info-name']/small/text()")[0].strip()
         creatorName = creatorName+patreonName
         creatorDirectory = os.path.join(DESTINATION, creatorName)
         failedLogPath = os.path.join(creatorDirectory, "failed_links.txt")
+        embededLogPath = os.path.join(creatorDirectory, "embeded_links.txt")
         os.makedirs(creatorDirectory, exist_ok=True)
+
         if CONTINUE:
             nextPage = pageTree.xpath("//a[@class='btn pag-btn pag-btn-bottom'][1]/@href")
             if nextPage: 
                 nextPage = nextPage[0]
                 index = suppliedLinks.index(suppliedLink) + 1
-                suppliedLinks.insert(index, nextPage)         
+                suppliedLinks.insert(index, nextPage)  
+                       
+        for post in posts:
 
-        allMedia = pageTree.xpath("//div[@class='card-attachments']//a/@href")
-        if not allMedia:
-            allMedia = pageTree.xpath("//div[@class='card-action']//a/@href")
+            cardAttachments = post.xpath(".//div[@class='card-attachments']//a/@href")
+            cardAction = post.xpath(".//div[@class='card-action']//a/@href") 
+            cardEmbed = post.xpath(".//div[@class='card-embed']//a/@href") 
 
-        for media in allMedia:
-
-            if SKIP in media:
-                continue
+            if cardAttachments:
+                allMedia = cardAttachments
+            else:
+                allMedia = cardAction
             
-            filename = f"{FILE_COUNTER}_{media.strip('/').split('/')[-1]}"
-            filepath = os.path.join(creatorDirectory, filename)
+            if cardEmbed:
+                writeFiles(cardEmbed, embededLogPath)
 
-            try:
-                fileData = session.head(media, headers=HEADERS, timeout=TIMEOUT)
-                try:
-                    fileSize = int(fileData.headers["Content-Length"])
-                except KeyError:
-                    fileSize = 0
-                if os.path.isfile(filepath):
-
-                    localFileSize = os.stat(filepath).st_size
-                    if localFileSize == fileSize:
-                        FILE_COUNTER += 1
-                        continue
-                    else:
-                        diff = int(fileSize) - int(localFileSize)
-                        if not diff == int(fileSize):
-                            HEADERS["Range"] = f"bytes={diff+1}-{fileSize-1}"
-
-                        fileResp = session.get(media, headers=HEADERS, stream=True, timeout=TIMEOUT)
-                        fileResp.raise_for_status()
-                else:
-                    fileResp = session.get(media, headers=HEADERS, stream=True, timeout=TIMEOUT)
-                    fileResp.raise_for_status()
-
-            except requests.exceptions.ConnectionError as connErr:
-                failedLinks(media, failedLogPath)
-                print(connErr)
-                FILE_COUNTER += 1
-                continue
+            for media in allMedia:
         
-            except (socket.timeout, ReadTimeoutError, requests.Timeout) as timeErr:
-                failedLinks(media, failedLogPath)
-                print(timeErr)
-                FILE_COUNTER += 1
-                continue
-        
-            except requests.exceptions.HTTPError as err:
-                failedLinks(media, failedLogPath)
-                print(err)
-                FILE_COUNTER += 1
-                continue
-        
-            
-            with open(filepath, "ab") as file:
-
-                print(ERASE, end="\r", flush=True)
-                print(f"[+] Downloading file: {filename}, have downloaded {FILE_COUNTER} files", end="\r", flush=True)
-
-                for iterData in fileResp.iter_content(chunk_size=2**20): 
-                    if iterData:
-                        file.write(iterData) 
+                if SKIP in media:
+                    continue
                 
-                FILE_COUNTER += 1
-            sleep(SLEEP)
+                filename = f"{FILENAME_COUNTER}_{media.strip('/').split('/')[-1]}"
+                filepath = os.path.join(creatorDirectory, filename)
+
+                try:
+                    fileData = session.get(media, headers=HEADERS, stream=True,timeout=TIMEOUT)
+                    
+                    try:
+                        fileSize = int(fileData.headers["Content-Length"])
+                    except KeyError:
+                        fileSize = 0
+                    if os.path.isfile(filepath):
+
+                        localFileSize = os.stat(filepath).st_size
+                        
+                        if localFileSize == fileSize:
+                            FILENAME_COUNTER += 1
+                            FILE_COUNTER += 1
+                            continue
+                        else:
+                            if fileSize == 0:
+                                HEADERS["Range"] = f"bytes={0}-"
+                            else:
+                                diff = int(fileSize) - int(localFileSize)
+                                HEADERS["Range"] = f"bytes={diff+1}-{fileSize-1}"
+
+                            fileResp = session.get(media, headers=HEADERS, stream=True, timeout=TIMEOUT)
+                            fileResp.raise_for_status()
+                    else:
+                        fileResp = fileData
+                        fileResp.raise_for_status()
+
+                except requests.exceptions.ConnectionError as connErr:
+                    writeFiles(media, failedLogPath)
+                    print(connErr)
+                    FILENAME_COUNTER += 1
+                    continue
+            
+                except (socket.timeout, ReadTimeoutError, requests.Timeout) as timeErr:
+                    writeFiles(media, failedLogPath)
+                    print(timeErr)
+                    FILENAME_COUNTER += 1
+                    continue
+            
+                except requests.exceptions.HTTPError as err:
+                    writeFiles(media, failedLogPath)
+                    print(err)
+                    FILENAME_COUNTER += 1
+                    continue
+            
+                with open(filepath, "ab") as file:
+
+                    print(ERASE, end="\r", flush=True)
+                    print(f"[+] Downloading file: {filename}, have downloaded {FILE_COUNTER} files", end="\r", flush=True)
+
+                    for iterData in fileResp.iter_content(chunk_size=2**20): 
+                        if iterData:
+                            file.write(iterData) 
+                    
+                    FILE_COUNTER += 1
+                    FILENAME_COUNTER += 1
+                sleep(SLEEP)
                     
 print(ERASE, end="\r", flush=True)
 print(f"[+] {FILE_COUNTER} files downloaded")
